aebba13dda30ed7fe4a141c8a4f7260277be463e
Karl Chelton <karl@absolute.agency>
2023-10-30 12:24:39 +0000

    Update server.js

--- server.js
+++ server.js
@@ -1,20 +1,27 @@
+let chalk
+
+import('chalk').then(module => {
+    chalk = module.default
+})
+
 const { Server } = require('socket.io')
 
-const io = new Server(3000, {
+const io = new Server(1337, {
     cors: {
-        origin: 'http://localhost:5173', // Your Vite server
+        origin: 'http://localhost:5173',
         methods: ['GET', 'POST']
     }
 })
 
 io.on('connection', socket => {
-    console.log('New client connected')
+    console.log(chalk.green('(+) Client connected to socket'))
 
     socket.on('send-message', message => {
         io.emit('message', message)
+        console.log(chalk.magenta(`(>) Message sent: ${message}`))
     })
 
     socket.on('disconnect', () => {
-        console.log('Client disconnected')
+        console.log(chalk.red('(-) Client disconnected from socket'))
     })
 })
